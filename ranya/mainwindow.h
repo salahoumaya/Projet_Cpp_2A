3b9b4760a0e6682e49518985293dc4b557aae113
ranya <ranya.azizikadmiri@esprit.tn>
2022-11-09 20:56:28 +0100

    18

---
+++ ranya/mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include"evenement.h"
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+    void on_prechercher_clicked();
+
+
+
+void on_recherchelieu_textChanged(const QString &arg1);
+
+    void on_comboBox_highlighted(const QString &arg1);
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_2_clicked();
+
+ void on_mdf_clicked();
+
+ void on_recherchertype_textChanged(const QString &arg1);
+
+private:
+    Ui::MainWindow *ui;
+    Evenement EV;
+};
+
+#endif // MAINWINDOW_H
